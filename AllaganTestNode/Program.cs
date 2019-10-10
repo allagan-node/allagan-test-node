e89ede7ed64b14e36e4796646134f90276d452a5
allagan-node <allagan.node@gmail.com>
2019-10-09 22:10:22 -0700

    file picker update

--- AllaganTestNode/Program.cs
+++ AllaganTestNode/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
+using System.IO;
 using System.Reflection;
+using System.Windows.Forms;
 
 namespace AllaganTestNode
 {
@@ -13,6 +15,7 @@ namespace AllaganTestNode
 
         private static bool backUpTarget = true;
 
+        [STAThread]
         static void Main(string[] args)
         {
             while (true)
@@ -20,16 +23,35 @@ namespace AllaganTestNode
                 PrintScreen();
 
                 string input = Console.ReadLine();
+                Console.WriteLine();
 
                 if (int.TryParse(input, out int _input) && _input > 0 && _input <= 6)
                 {
-
+                    switch (_input)
+                    {
+                        case 1:
+                            PickPath(ref sourceIndexPath, "READ from");
+                            break;
+                        case 2:
+                            PickLanguageCode(sourceIndexPath, ref sourceLanguage);
+                            break;
+                        case 3:
+                            PickPath(ref targetIndexPath, "WRITE to");
+                            break;
+                        case 4:
+                            PickLanguageCode(targetIndexPath, ref targetLanguage);
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                    }
                 }
                 else
                 {
-                    Console.WriteLine();
                     Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("");
+                    Console.WriteLine("Input is invalid. Press ENTER to continue.");
+                    Console.ReadLine();
                 }
             }
         }
@@ -43,38 +65,105 @@ namespace AllaganTestNode
             Console.WriteLine(string.Format("Allagan Test Node v{0}", Assembly.GetExecutingAssembly().GetName().Version.ToString()));
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("Pick an operation to execute:");
+            Console.WriteLine("Pick an operation to execute: ");
+            Console.WriteLine();
+
+            bool isBuildable = true;
 
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Write("[1] - Pick source file: ");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("[1] - Pick source file");
+            Console.Write("-> ");
+            isBuildable &= !string.IsNullOrEmpty(sourceIndexPath);
+            Console.ForegroundColor = string.IsNullOrEmpty(sourceIndexPath) ? ConsoleColor.DarkRed : ConsoleColor.DarkGreen;
             Console.WriteLine(string.IsNullOrEmpty(sourceIndexPath) ? "NOT SELECTED" : sourceIndexPath);
+            Console.WriteLine();
 
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Write("[2] - Pick source language code: ");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("[2] - Pick source language code");
+            Console.Write("-> ");
+            isBuildable &= sourceLanguage != ExHLanguage.Null;
+            Console.ForegroundColor = sourceLanguage == ExHLanguage.Null ? ConsoleColor.DarkRed : ConsoleColor.DarkGreen;
             Console.WriteLine(sourceLanguage == ExHLanguage.Null ? "NOT SELECTED" : sourceLanguage.ToString());
+            Console.WriteLine();
 
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Write("[3] - Pick target file: ");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("[3] - Pick target file");
+            Console.Write("-> ");
+            isBuildable &= !string.IsNullOrEmpty(targetIndexPath);
+            Console.ForegroundColor = string.IsNullOrEmpty(targetIndexPath) ? ConsoleColor.DarkRed : ConsoleColor.DarkGreen;
             Console.WriteLine(string.IsNullOrEmpty(targetIndexPath) ? "NOT SELECTED" : targetIndexPath);
+            Console.WriteLine();
 
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Write("[4] - Pick target language code: ");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("[4] - Pick target language code");
+            Console.Write("-> ");
+            isBuildable &= targetLanguage != ExHLanguage.Null;
+            Console.ForegroundColor = targetLanguage == ExHLanguage.Null ? ConsoleColor.DarkRed : ConsoleColor.DarkGreen;
             Console.WriteLine(targetLanguage == ExHLanguage.Null ? "NOT SELECTED" : targetLanguage.ToString());
+            Console.WriteLine();
 
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Write("[5] - Back up target file?: ");
-            Console.ForegroundColor = backUpTarget ? ConsoleColor.DarkGreen : ConsoleColor.DarkRed;
-            Console.WriteLine(backUpTarget.ToString().ToUpper());
+            Console.WriteLine("[5] - Back up target file?");
+            Console.Write("-> ");
+            Console.ForegroundColor = backUpTarget ? ConsoleColor.DarkGreen : ConsoleColor.Gray;
+            Console.WriteLine(backUpTarget ? "YES" : "NO");
+            Console.WriteLine();
 
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = isBuildable ? ConsoleColor.DarkGreen : ConsoleColor.DarkRed;
             Console.WriteLine("[6] - Build with above settings.");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("Choose an option: ");
+        }
+
+        static void PickPath(ref string path, string operation)
+        {
+            Console.Clear();
 
             Console.WriteLine();
-            Console.Write("Choose an option: ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(string.Format("Please choose the 0a0000.win32.index file that you will {0}.", operation));
+            Console.WriteLine("The file can usually be found under FFXIV\\game\\sqpack\\ffxiv");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Press ENTER to open the file picker...");
+            Console.ReadLine();
+
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (!string.IsNullOrEmpty(path) && File.Exists(path))
+                {
+                    ofd.InitialDirectory = Path.GetDirectoryName(path);
+                }
+
+                ofd.Filter = "0a0000 Index file (0a0000.win32.index)|0a0000.win32.index";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    path = ofd.FileName;
+                }
+            }
+        }
+
+        static void PickLanguageCode(string path, ref ExHLanguage language)
+        {
+            Console.Clear();
+            Console.WriteLine();
+
+            if (string.IsNullOrEmpty(path) || !File.Exists(path))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Specified index file path is incorrect. Please make sure you selected a file first!");
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("Press ENTER to go back to the main menu...");
+                Console.ReadLine();
+
+                return;
+            }
         }
     }
 }
